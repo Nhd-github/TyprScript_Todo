34d6b4cc83e0bf12962a7fe4417aaeda38031f70
Nhd-github <nhd.linkedin@gmail.com>
2024-09-16 01:06:32 -0700

    Update Todo.tsx

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt } from '@fortawesome/free-regular-svg-icons'
 import { Todo as TodoType } from './Todos.types'
